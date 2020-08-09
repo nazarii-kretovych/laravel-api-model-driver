e0711d188b3429ed220aa1d2c8217da8083ac4a0
Nazarii Kretovych <nazarii.kretovych@gmail.com>
2020-08-09 09:08:20 +0300

    Add support of Laravel Passport Client Credentials authentication.

--- src/Connection.php
+++ src/Connection.php
@@ -4,10 +4,13 @@ namespace NazariiKretovych\LaravelApiModelDriver;
 
 use Illuminate\Database\Connection as ConnectionBase;
 use Illuminate\Database\Grammar as GrammerBase;
+use Illuminate\Support\Facades\Cache;
 use RuntimeException;
 
 class Connection extends ConnectionBase
 {
+    const AUTH_TYPE_PASSPORT_CLIENT_CREDENTIALS = 'passport_client_credentials';
+
     /**
      * @return GrammerBase
      */
@@ -29,13 +32,14 @@ class Connection extends ConnectionBase
     {
         return $this->run($query, $bindings, function ($query) {
             // Get connection configuration.
-            $max_per_page = $this->getConfig('default_params')['per_page'];
+            $maxPerPage = $this->getConfig('default_params')['per_page'];
+            $maxUrlLength = $this->getConfig('max_url_length') ?: 4000;
 
             // Get full URL.
             $fullUrl = $this->getDatabaseName() . $query;
 
             // If the full URL is too long, we need to split it.
-            if (strlen($fullUrl) > 5000) {
+            if (strlen($fullUrl) > $maxUrlLength) {
                 // Parse query string and get params.
                 $questionIx = strpos($fullUrl, '?');
                 if ($questionIx === false) {
@@ -76,27 +80,26 @@ class Connection extends ConnectionBase
                 // Get data.
                 $json = $this->getJsonByUrl($url);
                 if (isset($json['current_page'])) {
+                    // There is pagination. We expect to receive data objects in the 'data' property.
                     foreach ($json['data'] as $row) {
                         $allRows[] = $row;
                     }
-                    $page = $json['current_page'];
-                    while (count($json['data']) === $max_per_page) {
-                        $page++;
-                        $nextUrl = preg_replace('#(\?|&)page=\d+#', "$1page=$page", $url, 1);
-                        if ($nextUrl === $url) {
-                            if (strpos($nextUrl, '?') !== false) {
-                                $nextUrl .= '&';
-                            } else {
-                                $nextUrl .= '?';
+
+                    // If the URL does not have the 'page' parameter, get data from all the pages.
+                    if (count($json['data']) >= $maxPerPage && !preg_match('#(\?|&)page=\d+#', $url)) {
+                        $page = $json['current_page'];
+                        $hasQueryString = (strpos($url, '?') !== false);
+                        while (count($json['data']) >= $maxPerPage) {
+                            $page++;
+                            $nextUrl = $url . ($hasQueryString ? '&' : '?') . "page=$page";
+                            $json = $this->getJsonByUrl($nextUrl);
+                            foreach ($json['data'] as $row) {
+                                $allRows[] = $row;
                             }
-                            $nextUrl .= "page=$page";
-                        }
-                        $json = $this->getJsonByUrl($nextUrl);
-                        foreach ($json['data'] as $row) {
-                            $allRows[] = $row;
                         }
                     }
                 } else {
+                    // No pagination.
                     foreach ($json as $row) {
                         $allRows[] = $row;
                     }
@@ -116,10 +119,47 @@ class Connection extends ConnectionBase
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_FAILONERROR, true);
         }
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getConfig('headers') ?: []);
+
+        // Set headers.
+        $headers = $this->getConfig('headers') ?: [];
+        $auth = $this->getConfig('auth');
+        if ($auth && $auth['type'] === self::AUTH_TYPE_PASSPORT_CLIENT_CREDENTIALS) {
+            // Try to retrieve the access token from cache.
+            $key = 'laravel_api_model_driver|' . $this->getDatabaseName() . '|token';
+            $tokenData = Cache::get($key);
+            $ts = time();
+            if (!$tokenData || $tokenData['expires'] + 3600 > $ts) {
+                // Get a new access token.
+                curl_setopt($ch, CURLOPT_URL, $auth['url']);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+                    'grant_type' => 'client_credentials',
+					'client_id' => $auth['client_id'],
+					'client_secret' => $auth['client_secret'],
+                ]));
+                curl_setopt($ch, CURLOPT_HTTPHEADER, []);
+                $result = curl_exec($ch);
+                if (!$result) {
+                    throw new RuntimeException('Failed to get access token from ' . $auth['url']);
+                }
+                $json = json_decode($result, true);
+                $tokenData = [
+                    'access_token' => $json['access_token'],
+                    'expires' => $ts + $json['expires_in'],
+                ];
+
+                // Cache the token.
+                Cache::put($key, $tokenData, $json['expires_in']);
+            }
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        // Set URL.
         curl_setopt($ch, CURLOPT_URL, $url);
 
         // Call API.
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, null);
         $result = curl_exec($ch);
         if (!$result) {
             throw new RuntimeException("Failed to call $url");
